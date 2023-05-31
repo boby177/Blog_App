357ef1a991f85739180c77f0dc35d6540a9b4196
bobym177 <boby.s@kiranatama.com>
2023-05-31 16:15:36 +0700

    update related component data and navigation

--- components/_child/related.js
+++ components/_child/related.js
@@ -1,84 +1,59 @@
 import Image from "next/image";
 import Link from "next/link";
 import Author from "./author";
-import Author2 from "./author2";
+import fetcher from "../../lib/fetcher";
+import Spinner from "../_child/spinner";
+import Error from "../_child/error";
 
 export default function related() {
+  const { data, isLoading, isError } = fetcher(`api/posts`);
+  if (isLoading) {
+    return <Spinner />;
+  } else if (isError) {
+    return <Error />;
+  }
+
   return (
     <section className="pt-20">
       <h1 className="font-bold text-3xl py-10 text-center">Related</h1>
 
       <div className="flex flex-col gap-10">
-        {Post()}
-        {Post2()}
-        {Post()}
-        {Post2()}
+        {data.map((value, index) => (
+          <Post key={index} data={value} />
+        ))}
       </div>
     </section>
   );
 }
 
-function Post() {
-  return (
-    <div className="flex gap-5">
-      <div className="image flex flex-col justify-start">
-        <Image
-          src={"/images/img3.png"}
-          className="rounded"
-          width={200}
-          height={150}
-        />
-      </div>
-      <div className="info flex justify-center flex-col">
-        <div className="cat">
-          <Link href={"/"}>
-            <div className="text-red-600 hover:text-red-500">Next JS </div>
-            <div className="text-gray-600 hover:text-gray-500">
-              July 3, 2022
-            </div>
-          </Link>
-        </div>
-        <div className="title">
-          <Link href={"/"}>
-            <div className="text-xl font-bold text-gray-600 hover:text-gray-600">
-              Material UI for React JS
-            </div>
-          </Link>
-        </div>
-        <Author />
-      </div>
-    </div>
-  );
-}
+function Post({ data }) {
+  const { id, title, description, category, img, published, author } = data;
 
-function Post2() {
   return (
     <div className="flex gap-5">
       <div className="image flex flex-col justify-start">
-        <Image
-          src={"/images/img4.png"}
-          className="rounded"
-          width={200}
-          height={150}
-        />
+        <Link href={`/posts/${id}`}>
+          <Image src={img || ""} className="rounded" width={200} height={150} />
+        </Link>
       </div>
       <div className="info flex justify-center flex-col">
         <div className="cat">
-          <Link href={"/"}>
-            <div className="text-red-600 hover:text-red-500">Next JS </div>
+          <Link href={`/posts/${id}`}>
+            <div className="text-red-600 hover:text-red-500">
+              {category || "Unknown"}
+            </div>
             <div className="text-gray-600 hover:text-gray-500">
-              July 3, 2022
+              {published || "Unknown"}
             </div>
           </Link>
         </div>
         <div className="title">
-          <Link href={"/"}>
+          <Link href={`/posts/${id}`}>
             <div className="text-xl font-bold text-gray-600 hover:text-gray-600">
-              Material UI for React JS
+              {title || "Title"}
             </div>
           </Link>
         </div>
-        <Author2 />
       </div>
     </div>
   );
