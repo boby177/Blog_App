342ab867ef7afff3fc9b2b7d5a290d04aa1a82ec
bobym177 <boby.s@kiranatama.com>
2023-05-31 13:43:39 +0700

    fetching and display all data from API

--- components/section-2.js
+++ components/section-2.js
@@ -1,56 +1,56 @@
 import Image from "next/image";
 import Link from "next/link";
 import Author from "../components/_child/author2";
+import fetcher from "../lib/fetcher";
 
 export default function section2() {
+  const { data, isLoading, isError } = fetcher(`api/posts`);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else if (isError) {
+    return <div>Error </div>;
+  }
+
   return (
     <section className="container mx-auto md:px-20 py-10">
       <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
       {/* Grid Columns */}
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
+        {data.map((value, index) => (
+          <Post data={value} key={index} />
+        ))}
       </div>
     </section>
   );
 }
 
-function Post() {
+function Post({ data }) {
+  const { id, title, subtitle, category, img, published, author } = data;
   return (
     <div className="item">
       <div className="images">
-        <Image
-          src={"/images/img4.png"}
-          className="rounded"
-          width={500}
-          height={350}
-        />
+        <Image src={img || "/"} className="rounded" width={500} height={350} />
       </div>
       <div className="info flex justify-center flex-col py-4">
         <div className="cat">
           <Link href={"/"}>
-            <div className="text-red-600 hover:text-red-500">Gadget </div>
+            <div className="text-red-600 hover:text-red-500">
+              {category || "Unknown"}
+            </div>
             <div className="text-gray-600 hover:text-gray-500">
-              July 3, 2022
+              {published || "Unknown"}
             </div>
           </Link>
         </div>
         <div className="title">
           <Link href={"/"}>
             <div className="text-xl font-bold text-gray-600 hover:text-gray-600">
-              Material UI for React JS
+              {title || "Title"}
             </div>
           </Link>
         </div>
-        <p className="text-gray-500 py-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-          risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
-          ultricies sed, dolor.
-        </p>
-        <Author />
+        <p className="text-gray-500 py-3">{subtitle || "Subtitle"}</p>
+        {author ? <Author /> : <></>}
       </div>
     </div>
   );
