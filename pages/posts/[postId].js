b76d9277598a820658acd236a43b1c00f9785816
bobym177 <boby.s@kiranatama.com>
2023-05-31 15:05:34 +0700

    fetching the data from post page component

---
+++ pages/posts/[postId].js
@@ -0,0 +1,55 @@
+import Format from "../../layouts/format";
+import Author from "@/components/_child/author";
+import Image from "next/image";
+import Related from "@/components/_child/related";
+import getPost from "@/lib/helper";
+
+export default function page({ title, img, subtitle, description, author }) {
+  return (
+    <Format>
+      <section className="container mx-auto md:px-2 py-16 w-1/2">
+        <div className="flex justify-center">{author ? <Author /> : <></>}</div>
+        <div className="post py-10">
+          <h1 className="font-bold text-4xl text-center pb-5">
+            {title || "Title"}
+          </h1>
+          <p className="text-gray-500 text-xl text-center">
+            {subtitle || "Subtitle"}
+          </p>
+          <div className="py-10">
+            <Image src={img} height={900} width={900} />
+          </div>
+          <div className="content text-gray-600 text-lg flex flex-col gap-4">
+            {description || "Description"}
+          </div>
+        </div>
+        <Related />
+      </section>
+    </Format>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const posts = await getPost(params.postId);
+
+  return {
+    props: posts,
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await getPost();
+
+  const paths = posts.map((value) => {
+    return {
+      params: {
+        postId: value.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
